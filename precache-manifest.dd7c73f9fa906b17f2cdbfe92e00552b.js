cb04f18fbaa9bc47e85c199648e342fa3cfa7fc2
notoriousarun <notoriousarun@protonmail.com>
2020-09-18 15:45:47 +0530

    Updates

---
+++ precache-manifest.dd7c73f9fa906b17f2cdbfe92e00552b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dad89b0e955f6d2ebf133e57b84a4c",
+    "url": "/hacker-news-search/index.html"
+  },
+  {
+    "revision": "7b0f6b4c0536038ac2bd",
+    "url": "/hacker-news-search/static/css/main.7e69a884.chunk.css"
+  },
+  {
+    "revision": "ab076c6cc2e3a740fa0f",
+    "url": "/hacker-news-search/static/js/2.df1db16b.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/hacker-news-search/static/js/2.df1db16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0f6b4c0536038ac2bd",
+    "url": "/hacker-news-search/static/js/main.d131e103.chunk.js"
+  },
+  {
+    "revision": "43d58b8d20f0bc252eb9",
+    "url": "/hacker-news-search/static/js/runtime-main.4ff96500.js"
+  }
+]);
